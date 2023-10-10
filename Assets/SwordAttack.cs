a58abda2ff787ba9f2df2737dcf502dbbe8e4182
aimalesa <aimalesa@icloud.com>
2023-10-10 17:04:00 -0600

    attack ability is now functional. -attack ability now has hitboxes that collide with objects tagged enemy -attack ability implemented with 4 directions -player stops moving when in act of attacking

--- Assets/SwordAttack.cs
+++ Assets/SwordAttack.cs
@@ -3,50 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordAttack : MonoBehaviour
-{
+{    
+    public BoxCollider2D swordCollider;
+    public float damage = 3;
     Vector2 currentPosition;
-    BoxCollider2D swordCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        swordCollider = GetComponent<BoxCollider2D>();
         Vector2 currentPosition = transform.position;
 
         // default to attack down 
-        transform.position = new Vector2(currentPosition.x + 0f, currentPosition.y + -0.2f);
-        swordCollider.size = new Vector2(0.25f, 0.1f);
+        // transform.localPosition = new Vector2(currentPosition.x + 0f, currentPosition.y + -0.2f);
+        // swordCollider.size = new Vector2(0.25f, 0.1f);
     }
 
     public void AttackUp(){
         swordCollider.enabled = true;
         transform.localPosition = new Vector2(currentPosition.x + 0f, currentPosition.y + -0.05f);
         swordCollider.size = new Vector2(0.25f, 0.15f);
-        print("up");
     }
     public void AttackDown(){
         swordCollider.enabled = true;
         transform.localPosition = new Vector2(currentPosition.x + 0f, currentPosition.y + -0.2f);
         swordCollider.size = new Vector2(0.25f, 0.1f);
-        print("down");        
     }
 
     public void AttackLeft(){
         swordCollider.enabled = true;
         transform.localPosition = new Vector2(currentPosition.x + -0.1f, currentPosition.y + -0.1f);
         swordCollider.size = new Vector2(0.2f, 0.25f);
-        print("left");
     }
 
     public void AttackRight(){
         swordCollider.enabled = true;
         transform.localPosition = new Vector2(0.1f, -0.1f);
         swordCollider.size = new Vector2(0.2f, 0.25f);
-        print("right");
     }
 
     public void StopAttack(){
         swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Enemy"){
+            Enemy enemy = other.GetComponent<Enemy>();
+            if(enemy != null){
+                enemy.Health -= damage;
+            }
+        }
     }
 
     // Update is called once per frame
