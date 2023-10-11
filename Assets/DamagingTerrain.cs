3cffca034d145703a1b87f85eb8cda5da2e6c0f0
aimalesa <aimalesa@icloud.com>
2023-10-11 17:29:43 -0600

    Added player health, damage, and death - Player can now take damage - Player now has a health value - Player can now die

---
+++ Assets/DamagingTerrain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagingTerrain : MonoBehaviour
+{
+
+    public float damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            PlayerController player = other.GetComponent<PlayerController>();
+            if(player != null){
+                player.Health -= damage;
+            }
+        }
+    }
+}
